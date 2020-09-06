452a11e56a9f686b6b5d83e7227f21fe765d8c85
echrs <echrs@outlook.com>
2020-09-06 23:35:06 +0200

    attempts to fix message refresh, dialog when user is not registered, prep for updating msg fields

--- src/services/index.js
+++ src/services/index.js
@@ -67,6 +67,10 @@ let Rooms = {
                 roomId: doc._id,
                 roomName: doc.name,
                 users: doc.users,
+                businessKey: doc.businessKey,
+                processInstanceId: doc.processInstanceId,
+                definitionId: doc.definitionId,
+                variables: doc.variables,
             };
         });
     },
@@ -77,25 +81,13 @@ let Rooms = {
                 roomId: doc._id,
                 roomName: doc.name,
                 users: doc.users,
+                businessKey: doc.businessKey,
+                processInstanceId: doc.processInstanceId,
+                definitionId: doc.definitionId,
+                variables: doc.variables,
             };
         });
     },
-
-    updateUserField(user, field, value) {
-        Service.post('/api/updateUserField', {
-            user: user,
-            field: field,
-            value: value
-        })
-            .then(
-                response => {
-                    response;
-                },
-                error => {
-                    console.log(error);
-                }
-            );
-    }
 }
 
 let Messages = {
@@ -156,6 +148,21 @@ let Messages = {
                 }
             );
 
+    },
+    updateMessageField(message, field, value) {
+        Service.post('/api/updateMessageField', {
+            message: message,
+            field: field,
+            value: value
+        })
+            .then(
+                response => {
+                    response;
+                },
+                error => {
+                    console.log(error);
+                }
+            );
     }
 }
 
@@ -166,9 +173,10 @@ let Users = {
             return {
                 id: doc._id,
                 username: doc.username,
-                group: doc.group,
+                firstName: doc.firstName,
+                lastName: doc.lastName,
                 chatRooms: doc.chatRooms,
-                messages: doc.messages
+                selectedRoom: doc.selectedRoom
             };
         });
     },
@@ -178,18 +186,29 @@ let Users = {
             return {
                 id: doc._id,
                 username: doc.username,
-                group: doc.group,
+                firstName: doc.firstName,
+                lastName: doc.lastName,
                 chatRooms: doc.chatRooms,
-                messages: doc.messages
+                selectedRoom: doc.selectedRoom
             };
         });
+    },
+    
+    updateUserField(user, field, value) {
+        Service.post('/api/updateUserField', {
+            user: user,
+            field: field,
+            value: value
+        })
+            .then(
+                response => {
+                    response;
+                },
+                error => {
+                    console.log(error);
+                }
+            );
     }
 }
-
-// let Files = {
-//     getAll() {
-//         return Service.get('/api/getFiles')
-//     }
-// }
 
 export { Service, Rooms, Messages, Users, Camunda, Auth }
