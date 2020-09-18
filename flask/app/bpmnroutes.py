21419a8915bdad89857e774e605b9afe28e9d6d8
starcictoni <57995236+starcictoni@users.noreply.github.com>
2020-09-18 15:55:17 +0200

    Added prints to bpmnroutes

--- flask/app/bpmnroutes.py
+++ flask/app/bpmnroutes.py
@@ -52,20 +52,21 @@ def get_task_variables(user):
     
     if json.loads(dbroutes.get_selected_room(user)) != []:
         selected_room = json.loads(dbroutes.get_selected_room(user))[0]
-        print("Odabrana soba: ", selected_room)
+        print("Odabrana soba, moze doci do problema kada profesor ima dvije prijave zavrsnog: ", selected_room)
         if selected_room['name'] == 'Recepcija' or selected_room == None:
             return "Nema pokrenutih procesa" 
         instance_id = selected_room['processInstanceId']
         definition_id = selected_room['definitionId']
         business_key = selected_room['businessKey']
         variables = json.loads(dbparser.parse_db_data(user, selected_room['variables']))
-        print("Parsirane varijable: ", variables)
+        print("Ovdje izlaze DB varijable za front: ", variables)
         flag = selected_room['flag']
         room_id = selected_room['_id']
-
+        print("Flag odabrane sobe: ", flag)
+        print("Room_ID odabrane sobe: ", room_id)
         try:
             status = json.loads(camundarest.check_process_instance_status(instance_id, business_key, definition_id))[0]['state']
-            print("Status: ", status)
+            print("Provjera statusa procesne instance, ako je ACTIVE, nastavi s radom: ", status)
         except Exception as error:
             return json.dumps({'Error': str(error)})
 
@@ -73,7 +74,7 @@ def get_task_variables(user):
             if camundarest.get_user_task(definition_id, instance_id) != '[]':
                 try:
                     current_task = json.loads(camundarest.get_user_task(definition_id, instance_id))[0]
-                    print("Task je user task")
+                    print("Provjera jel trenutni task- User task")
                 except Exception as error:
                     return json.dumps({'Error': str(error)})
                 finally:           
@@ -82,7 +83,7 @@ def get_task_variables(user):
             else:
                 try:
                     current_task = json.loads(camundarest.get_external_task(definition_id, instance_id))[0]
-                    print("Task je external task")
+                    print("Provjera jel trenutni task- External task")
                 except Exception as error:
                     return json.dumps({'Error': str(error)})
                 finally:
@@ -95,57 +96,64 @@ def get_task_variables(user):
             if task_assignee != None:    
                 print("Task Assignee postoji")
                 if task_assignee == user:
-                    print("Task Assignee je user")               
+                    print("Task Assignee je User")               
                     if task_form_key != None:
                         try:
-                            print("2")
-                            task_variables = xmlparser.parse(definition_id, task_form_key)
+                            print("Task Form Key postoji, što znači da postoje varijable u Camundi koje treba preuzeti")
+                            task_variables = json.loads(xmlparser.parse(definition_id, task_form_key))
                         except Exception as error:
                             return {'Error': str(error)}    
-                        if variables != '[]':
-                            print("3")
+                        if variables != []:
+                            print("Varijable u bazi nisu prazne, pa slijedi provjera")
                             if flag == False:
                                 try:            
-                                    print("4")  
+                                    print("Flag je false što znači da ce povuci te varijable iz baze samo jednom")  
                                     mongo.db.chatRooms.update_one({"_id": ObjectId(room_id['$oid'])}, {'$set': {'flag': True}})
                                 except Exception as error:
                                     return json.dumps({'Error': str(error)})
                                 finally:
-                                    print("5")
-                                    print("Varijable unutar flag == False: ", variables)
-                                    print("task_variables: ", task_variables)
                                     data = {
                                         "databaseVariables": variables,
                                         "serviceVariables": task_variables,
                                     }
+                                    print("Vraca i DB i Camunda varijable: ", data)
                                     return data
-                            else:                           
+                            else:
+                                print("Varijable iz baze su povucene, pa vraca samo one iz Camunde: ", task_variables)                        
                                 return task_variables
                         else:
+                            #Ovo ce se vjerojatno izvrsiti samo jedanput
+                            print("Varijable u bazi su prazne, pa vraca samo one iz Camunde: ", task_variables)
                             return task_variables
                     else:
-                        if variables != []:                               
-                            if flag == False:                   
+                        print("Form Key je prazan sto znaci da nema varijabli u Camundi")
+                        if variables != []:
+                            print("Provjera jel Flag == false")                               
+                            if flag == False:
+                                print("Varijable u Camundi su prazne, ali je doslo do ovdje kako bi uzeo varijable iz Baze- skoro pa edgecase")                  
                                 try:              
                                     mongo.db.chatRooms.update_one({"_id": ObjectId(room_id['$oid'])}, {'$set': {'flag': True}})
                                 except Exception as error:
                                     return json.dumps({'Error': str(error)})
-                                data = {
-                                    "databaseVariables": variables,
-                                    "serviceVariables": task_variables,
-                                }
-                                return data
-                            else:                              
+                                finally:
+                                    return variables
+                            else:
+                                print("Totalni edgecase, u Camundi su prazne varijable, a iz Baze vec povucene")                              
                                 return "Chat logika" #Varijable su vec povucene iz baze
                         else:
+                            print("Osim sto su u Camundi prazne varijable, prazne su i u bazi. To je onaj task gdje profesor i student chataju")
                             return "Chat logika" #Varijable ne postoje
-                else: 
+                else:
+                    print("Trenutno ulogirani korisnik nije assignee")
                     return "Task mora odraditi druga osoba"
 
             else:
+                print("Assignee ne postoji, sto znaci da trenutni user nije assignee ili da je external task")
                 if task_form_key != None:
+                    print("Trenutno ulogirani korisnik nije assignee")
                     return "Task mora odraditi druga osoba"
                 else:
+                    print("Trentuni task radi stroj, tj. task je external")
                     if external_topic_name == 'izracunaj_skolarinu':
                         response = externals.izracunaj_skolarinu(external_task_id, external_topic_name, external_worker_id, variables)
                     elif external_topic_name == 'upisi_studenta':
@@ -154,6 +162,7 @@ def get_task_variables(user):
                         response = externals.unos_prijave(external_task_id, external_topic_name, external_worker_id, variables, user)
                     return response
         else:
+            print("Proces je zavsen i nema vise taskova")
             try:
                 mongo.db.chatRooms.update_one({"_id": ObjectId(room_id['$oid'])}, {'$set': {'active': False}})
             except Exception as error:
@@ -204,55 +213,55 @@ def get_assignee(user):
     return assignee
 
 #DATABASE VARS INTO MESSAGES FORMATTER
-@app.route('/api/<user>/task/variables/format', methods=['GET'])
-@cross_origin()
-def make_messages_out_of_database_variables(user):
-    selected_room = mongo.db.users.find_one({"username": user})['selectedRoom']
-    room = list(mongo.db.chatRooms.find({"_id": ObjectId(selected_room)}))[0]
-    flag = room['flag']
-    if flag != True:
-        variable_names = []
-        props_list = []
-        for i in room['variables']: #prodi kroz varijable
-            variable_name = i.keys()       #uzmi nazive varijabli, pr. "NaslovRada"
-            variable_props = i.values()       #uzmi svojstva varijable, pr. {"value": "blabla", "type": "String"}
-            for index in variable_name:    
-                element = ''
-                for k, l in enumerate(index):           #ovo sluzi za stavljanje razmaka u usernameu, k je proslo slovo, l je trenutno slovo
-                    if k and l.isupper():
-                        element += ' '
-                    element += l
-                variable_names.append(element)              #dodaj element u polje varijabli imena 
-            for index in variable_props:                        
-                properties = (list(index.values())[0])             
-                props_list.append(properties)               #dodaj element u polje varijabli svojstava
+# @app.route('/api/<user>/task/variables/format', methods=['GET'])
+# @cross_origin()
+# def make_messages_out_of_database_variables(user):
+#     selected_room = mongo.db.users.find_one({"username": user})['selectedRoom']
+#     room = list(mongo.db.chatRooms.find({"_id": ObjectId(selected_room)}))[0]
+#     flag = room['flag']
+#     if flag != True:
+#         variable_names = []
+#         props_list = []
+#         for i in room['variables']: #prodi kroz varijable
+#             variable_name = i.keys()       #uzmi nazive varijabli, pr. "NaslovRada"
+#             variable_props = i.values()       #uzmi svojstva varijable, pr. {"value": "blabla", "type": "String"}
+#             for index in variable_name:    
+#                 element = ''
+#                 for k, l in enumerate(index):           #ovo sluzi za stavljanje razmaka u usernameu, k je proslo slovo, l je trenutno slovo
+#                     if k and l.isupper():
+#                         element += ' '
+#                     element += l
+#                 variable_names.append(element)              #dodaj element u polje varijabli imena 
+#             for index in variable_props:                        
+#                 properties = (list(index.values())[0])             
+#                 props_list.append(properties)               #dodaj element u polje varijabli svojstava
 
-        #ovdje se konkateniraju imena i svojstva u jedan objekt koji se kasnije appenda u listu, 1 element liste = 1 message content
-        object_concat = {}                                  
-        list_of_content = []
-        for index in range(len(variable_names)):
-            object_concat = str(variable_names[index]) + ": " + str(props_list[index]) #ovo je content
-            list_of_content.append(object_concat)
+#         #ovdje se konkateniraju imena i svojstva u jedan objekt koji se kasnije appenda u listu, 1 element liste = 1 message content
+#         object_concat = {}                                  
+#         list_of_content = []
+#         for index in range(len(variable_names)):
+#             object_concat = str(variable_names[index]) + ": " + str(props_list[index]) #ovo je content
+#             list_of_content.append(object_concat)
 
-        messages = []
-        temporary = {}
-        sender_id = mongo.db.users.find_one({"username": "Fi"})["_id"]
-        room_id = selected_room
-        timestamp = datetime.datetime.now().isoformat() + "Z"
+#         messages = []
+#         temporary = {}
+#         sender_id = mongo.db.users.find_one({"username": "Fi"})["_id"]
+#         room_id = selected_room
+#         timestamp = datetime.datetime.now().isoformat() + "Z"
 
-        for key in range(len(list_of_content)):
-            temporary = {
-                "room_id": room_id,
-                "sender_id": sender_id,
-                "username": "Fi",
-                "content": list_of_content[key],
-                "timestamp": timestamp,
-                "seen": False
-            }
-            messages.append(temporary)
-        return json.dumps(messages, default=json_util.default, sort_keys=False)
-    else:
-        return "Varijable su iz baze vec povucene"
+#         for key in range(len(list_of_content)):
+#             temporary = {
+#                 "room_id": room_id,
+#                 "sender_id": sender_id,
+#                 "username": "Fi",
+#                 "content": list_of_content[key],
+#                 "timestamp": timestamp,
+#                 "seen": False
+#             }
+#             messages.append(temporary)
+#         return json.dumps(messages, default=json_util.default, sort_keys=False)
+#     else:
+#         return "Varijable su iz baze vec povucene"
 
 
 
